5762ae61f87b49245adca474c82471169a3ea48e
Dzook1 <147112512+Dzook1@users.noreply.github.com>
2024-04-11 08:34:15 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function createTestExit() {
-    createTestBox = document.getElementsByClassName('createTest'[0]);
+    let createTestBox = document.getElementsByClassName('createTest')[0];
     createTestBox.style.display = 'none';
+    let testGrid = document.getElementsByClassName('tests-grid')[0];
+    testGrid.style.display = 'grid';
+}
+
+function displayCreateTest() {
+    let testGrid = document.getElementsByClassName('tests-grid')[0];
+    testGrid.style.display = 'none';
+    let createTestBox = document.getElementsByClassName('createTest')[0];
+    createTestBox.style.display = 'block';
 }
