2812ceb33a41b4b8d44115a18bbf294522a3df55
flight04 <xshefcyk4633@stevenscollege.edu>
2024-04-11 08:07:43 -0400

    ok

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-
+from sqlalchemy import create_engine, text
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+conn_str = 'mysql://root:Savier010523$@localhost/exams'
 
 
 @app.route('/')
