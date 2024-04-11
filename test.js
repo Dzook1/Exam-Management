a4f5bca952b30eda13ef3cf1bafae20581973c6a
Peyton Meyer <peytonmeyer23@gmail.com>
2024-04-11 09:57:40 -0400

    Test ui

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-function createTestExit() {
+function TestOpen() {
     takeTestBox = document.getElementsByClassName('takeTest-box')[0];
     takeTestBox.style.display = 'block';
+}
+function createTestExit() {
+    let createTestBox = document.getElementsByClassName('takeTest-box')[0];
+    createTestBox.style.display = 'none';
+
 }
