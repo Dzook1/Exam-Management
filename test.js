8850688422c30f5dcc2006252b6e3fd550e20bf3
Dzook1 <147112512+Dzook1@users.noreply.github.com>
2024-04-12 12:59:56 -0400

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-function TestOpen() {
-    takeTestBox = document.getElementsByClassName('takeTest-box')[0];
-    takeTestBox.style.display = 'block';
-}
-function createTestExit() {
-    let createTestBox = document.getElementsByClassName('takeTest-box')[0];
-    createTestBox.style.display = 'none';
-
-}
